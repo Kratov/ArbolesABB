16c54d4589e1d3972415805a05c369dac65d30eb
OrbisCorporation <mrjanzx@gmail.com>
2018-09-23 23:36:15 -0500

    Terminado. V1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-#include<random>
+#include<cctype>
 
 using namespace  std;
 
@@ -17,28 +18,91 @@ void mostrar(Nodo * arbol, int espaciado);
 void preOrder(Nodo * arbol);
 void inOrder(Nodo * arbol);
 void postOrder(Nodo * arbol);
-void eliminar(Nodo *&arbol);
+bool esUnNumero(const char* cadena);
+bool seguirIngresando();
+char mainMenu();
+int pedirNumero();
 
 
 int main() {
-	int datos[] = {8,3,1,6,4,7,10,14,13};
+	char op = '0';
 	Nodo * arbol = NULL;
-	random_device rd;
-	mt19937 engine(rd());
-	uniform_int_distribution<int> intDistribution(0, 99);
-	intDistribution(engine);
-	for (int i = 0; i < 9; i++)
-		insertar(arbol, datos[i]/*intDistribution(engine)*/, NULL);
-	mostrar(arbol, 0);
-	preOrder(arbol);
-	printf("\n");
-	inOrder(arbol);
-	printf("\n");
-	postOrder(arbol);
-	printf("\n");
-	system("PAUSE");
+	do {
+		system("CLS");
+		if (arbol) {
+			printf("\n	============ ARBOL ABB ============	\n\n");
+			mostrar(arbol, 0);
+			printf("\n	============ FIN ARBOL ABB ============	\n\n");
+		}
+		switch (op = mainMenu())
+		{
+		case '1':
+				insertar(arbol, pedirNumero(), NULL);
+			break;
+		case '2':
+			printf("\n	============ PREORDER ============	\n\n");
+			preOrder(arbol);
+			printf("\n\n	============ FIN PREORDER ============	\n\n");
+			break;
+		case '3':
+			printf("\n	============ INORDER ============	\n\n");
+			inOrder(arbol);
+			printf("\n\n	============ FIN INORDER ============	\n\n");
+			break;
+		case '4':
+			printf("\n	============ POSTORDER ============	\n\n");
+			postOrder(arbol);
+			printf("\n\n	============ FIN POSTORDER ============	\n\n");
+			break;
+		}
+		printf("\n");
+		system("PAUSE");
+	} while (op != '0');
 	return 0;
 }
+
+char mainMenu()
+{
+	char temp = '0';
+	printf("\n	PROGRAMA DE LECTURA DE ARBOLES ABB\n	1. Insertar	\n	2. Leer PreOrden  (RID)	\n	3. Leer InOrden   (IRD)	\n	4. Leer PostOrden (IDR)	\n	0. Salir\n	Seleccion: ");
+	scanf(" %c", &temp);
+	return temp;
+}
+
+bool seguirIngresando()
+{
+	char temp = '0';
+	do {
+		printf("\n	Desea seguir ingresando valores al arbol (Y/N)?	");
+		scanf(" %c", &temp);
+		switch (temp)
+		{
+		case 'Y':
+			return true;
+		case 'N':
+			return false;
+		}
+	} while (temp != 'Y' || temp != 'N');
+}
+
+bool esUnNumero(const char* cadena)
+{
+	for (; *cadena; ++cadena)
+		if (!std::isdigit(*cadena))
+			return false;
+	return true;
+}
+
+int pedirNumero()
+{
+	char temp[255];
+	do {
+		printf("\n	Ingrese numero: ");
+		scanf(" %s", temp);
+	} while (!esUnNumero(temp));
+	return atoi(temp);
+}
+
 
 void insertar(Nodo *&arbol,  const int n, Nodo *padre)
 {	
@@ -66,20 +130,19 @@ void mostrar(Nodo *arbol, int espaciado)
 	{
 		mostrar(arbol->der, espaciado+1);
 		for (int i = 0; i < espaciado; i++)		
-			printf("     ");
-		printf("%d\n", arbol->dato);
+			printf("	");
+		printf("	%d\n", arbol->dato);
 		mostrar(arbol->izq, espaciado+1);
+		
 	}
 }
 
 void preOrder(Nodo * arbol) {
 	if (!arbol)
-	{
 		return;
-	}
 	else
 	{
-		printf("%d ", arbol->dato);
+		printf("	%d ", arbol->dato);
 		preOrder(arbol->izq);
 		preOrder(arbol->der);
 	}
@@ -87,33 +150,23 @@ void preOrder(Nodo * arbol) {
 
 void inOrder(Nodo * arbol) {
 	if (!arbol)
-	{
 		return;
-	}
 	else
 	{
 		inOrder(arbol->izq);
-		printf("%d ", arbol->dato);
+		printf("	%d ", arbol->dato);
 		inOrder(arbol->der);
 	}
 }
 
 void postOrder(Nodo * arbol) {
 	if (!arbol)
-	{
 		return;
-	}
 	else
 	{
 		postOrder(arbol->izq);
 		postOrder(arbol->der);
-		printf("%d ", arbol->dato);
+		printf("	%d ", arbol->dato);
 	}
 }
-
-void eliminar(Nodo *&arbol)
-{
-
-}
-
 
